55646129283672d14b2069f60ea4493c7251b599
kikerless <2012kiker2012@gmail.com>
2021-07-12 17:48:29 +0300

    updated for product_page

--- pages/locators.py
+++ pages/locators.py
@@ -11,3 +11,9 @@ class LoginPageLocators:
     LOGIN_FORM = (By.ID, "login_form")
     REGISTRATION_FORM = (By.ID, "register_form")
 
+
+class ProductPageLocators:
+    ADD_TO_CART = (By.CLASS_NAME, "btn-add-to-basket")
+    PRODUCT_ADDED_TO_CART_MESSAGE = (By.XPATH, "/html/body/div[2]/div/div[1]/div[1]/div/strong")
+    PRODUCT_ADDED_PRICE_MESSAGE = (By.XPATH, "/html/body/div[2]/div/div[1]/div[3]/div/p[1]/strong")
+    SELLING_PRICE = (By.XPATH, "/html/body/div[2]/div/div[2]/div[2]/article/div[1]/div[2]/p[1]")
